02df3d13e43258e9ee9c1322fefd31a9d5e23085
Josue_Castillozz <jireyes@uamv.edu.ni>
2025-09-19 10:51:03 -0600

    Add files via upload

---
+++ Ejer-1-Sem-4/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Calcular el area de un triángulo
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la base del triángulo:");
+        double baseT = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del triángulo:");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double areaTriangulo = (baseT * altura) / 2;
+
+        Console.WriteLine($"El área del triángulo es: {areaTriangulo}");
+    }
+
+    static double CalcularAreaTriangulo(double baseT, double altura)
+    {
+        return (baseT * altura) / 2;
+    }
+}
