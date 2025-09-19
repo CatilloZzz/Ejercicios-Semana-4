334721f2f00f7e55aaf17633e07815172cb6388a
Josue_Castillozz <jireyes@uamv.edu.ni>
2025-09-19 10:29:04 -0600

    Add files via upload

---
+++ Ejercicio-3-Semana-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un número entero:");
+        int numero = int.Parse(Console.ReadLine());
+        
+        if (EsPar(numero))
+        {
+            Console.WriteLine($"{numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} es impar.");
+        }
+    }
+
+
+    static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
+
